1323e553cc2531e49c2d8cde448112c73edaf948
Le Vady <levady@spgroup.com.sg>
2019-06-28 13:23:38 +0800

    Refactor homepage handler to be a receiver function
    Since we only store sites in memory (in checker service), we need to pass a single instance of checker service to the handlers.

--- cmd/gohealth/httphandlers/sitehealthhandler.go
+++ cmd/gohealth/httphandlers/sitehealthhandler.go
@@ -3,10 +3,17 @@ package httphandlers
 import (
 	"html/template"
 	"net/http"
+
+	"github.com/levady/gohealth/internal/sitehealthchecker"
 )
 
+// SiteHealthHandler represents SiteHealthHandler data
+type SiteHealthHandler struct {
+	Checker *sitehealthchecker.SiteHealthChecker
+}
+
 // Homepage renders the home page
-func Homepage(w http.ResponseWriter, r *http.Request) {
+func (handler *SiteHealthHandler) Homepage(w http.ResponseWriter, r *http.Request) {
 	t, _ := template.ParseFiles("web/templates/homepage.html")
 	t.Execute(w, nil)
 }
--- cmd/gohealth/main.go
+++ cmd/gohealth/main.go
@@ -3,11 +3,18 @@ package main
 import (
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/levady/gohealth/cmd/gohealth/httphandlers"
+	"github.com/levady/gohealth/internal/sitehealthchecker"
 )
 
 func main() {
-	http.HandleFunc("/", httphandlers.Homepage)
+	shc := sitehealthchecker.New(15 * time.Second)
+	shh := httphandlers.SiteHealthHandler{
+		Checker: &shc,
+	}
+
+	http.HandleFunc("/", shh.Homepage)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
